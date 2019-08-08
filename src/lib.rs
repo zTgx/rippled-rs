f2d762adef7542f0a58c409dbfe147cc6d9f6dc4
zTgx <747674262@qq.com>
2019-08-08 17:57:23 +0800

    init project

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+extern crate ed25519_dalek;
+
+pub fn wallet_propose() -> Result<&'static str, &'static str> {
+
+    Ok("ok")
+}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    rippled_rs::wallet_propose().unwrap();
+    println!("rippled's signing algorithms in Rust.");
+}
